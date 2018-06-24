eaea653d104fd6ad5450867509780ddc13f88674
ericong18 <ericong18@gmail.com>
2018-06-23 21:30:12 -0700

    Weather icon dynamically changes with respect to weather conditions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,53 @@ import Weather from "./components/Weather";
  
 const API_KEY = process.env.REACT_APP_API_KEY;
 
+const weatherImages = [
+  require('./imgs/weather-icons/1.png'),
+  require('./imgs/weather-icons/2.png'),
+  require('./imgs/weather-icons/3.png'),
+  require('./imgs/weather-icons/4.png'),
+  require('./imgs/weather-icons/5.png'),
+  require('./imgs/weather-icons/6.png'),
+  require('./imgs/weather-icons/7.png'),
+  require('./imgs/weather-icons/8.png'),
+  require('./imgs/weather-icons/8.png'),
+  require('./imgs/weather-icons/8.png'),
+  require('./imgs/weather-icons/11.png'),
+  require('./imgs/weather-icons/12.png'),
+  require('./imgs/weather-icons/13.png'),
+  require('./imgs/weather-icons/14.png'),
+  require('./imgs/weather-icons/15.png'),
+  require('./imgs/weather-icons/16.png'),
+  require('./imgs/weather-icons/17.png'),
+  require('./imgs/weather-icons/18.png'),
+  require('./imgs/weather-icons/19.png'),
+  require('./imgs/weather-icons/20.png'),
+  require('./imgs/weather-icons/21.png'),
+  require('./imgs/weather-icons/22.png'),
+  require('./imgs/weather-icons/23.png'),
+  require('./imgs/weather-icons/24.png'),
+  require('./imgs/weather-icons/25.png'),
+  require('./imgs/weather-icons/26.png'),
+  require('./imgs/weather-icons/26.png'),
+  require('./imgs/weather-icons/26.png'),
+  require('./imgs/weather-icons/29.png'),
+  require('./imgs/weather-icons/30.png'),
+  require('./imgs/weather-icons/31.png'),
+  require('./imgs/weather-icons/32.png'),
+  require('./imgs/weather-icons/33.png'),
+  require('./imgs/weather-icons/34.png'),
+  require('./imgs/weather-icons/35.png'),
+  require('./imgs/weather-icons/36.png'),
+  require('./imgs/weather-icons/37.png'),
+  require('./imgs/weather-icons/38.png'),
+  require('./imgs/weather-icons/39.png'),
+  require('./imgs/weather-icons/40.png'),
+  require('./imgs/weather-icons/41.png'),
+  require('./imgs/weather-icons/42.png'),
+  require('./imgs/weather-icons/43.png'),
+  require('./imgs/weather-icons/44.png')
+];
+
 class App extends React.Component {
 
   state = {
@@ -36,6 +83,7 @@ class App extends React.Component {
       console.log(CURRENT_DATA[0].WeatherText);
       this.setState({
         weatherStatus: CURRENT_DATA[0].WeatherText,
+        iconNumber: CURRENT_DATA[0].WeatherIcon,
         currentTempF: CURRENT_DATA[0].Temperature.Imperial.Value,
         currentTempC: CURRENT_DATA[0].Temperature.Metric.Value
       });
@@ -76,6 +124,7 @@ class App extends React.Component {
                 area = { this.state.area }
                 country = { this.state.country }
                 weatherStatus = { this.state.weatherStatus }
+                weatherImages = { weatherImages }
                 iconNumber = { this.state.iconNumber }
                 currentTempF = { this.state.currentTempF }
                 currentTempC = { this.state.currentTempC }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,14 +1,70 @@
 import React from "react";
 import './Weather.css';
 
-const Weather = props => {
-  return(
-    <div>
-      { props.name && props.area && props.country && <h4> Results for { props.name }, { props.area }, { props.country }</h4> }
-      { props.high && <p className="temperature"> High: { props.high } &deg;F</p> }
-      { props.low && <p className="temperature"> Low: { props.low } &deg;F</p> }
-    </div>
-  );
+// const weatherImages = [
+//   require('../imgs/weather-icons/1.png'),
+//   require('../imgs/weather-icons/2.png'),
+//   require('../imgs/weather-icons/3.png'),
+//   require('../imgs/weather-icons/4.png'),
+//   require('../imgs/weather-icons/5.png'),
+//   require('../imgs/weather-icons/6.png'),
+//   require('../imgs/weather-icons/7.png'),
+//   require('../imgs/weather-icons/8.png'),
+//   require('../imgs/weather-icons/8.png'),
+//   require('../imgs/weather-icons/8.png'),
+//   require('../imgs/weather-icons/11.png'),
+//   require('../imgs/weather-icons/12.png'),
+//   require('../imgs/weather-icons/13.png'),
+//   require('../imgs/weather-icons/14.png'),
+//   require('../imgs/weather-icons/15.png'),
+//   require('../imgs/weather-icons/16.png'),
+//   require('../imgs/weather-icons/17.png'),
+//   require('../imgs/weather-icons/18.png'),
+//   require('../imgs/weather-icons/19.png'),
+//   require('../imgs/weather-icons/20.png'),
+//   require('../imgs/weather-icons/21.png'),
+//   require('../imgs/weather-icons/22.png'),
+//   require('../imgs/weather-icons/23.png'),
+//   require('../imgs/weather-icons/24.png'),
+//   require('../imgs/weather-icons/25.png'),
+//   require('../imgs/weather-icons/26.png'),
+//   require('../imgs/weather-icons/26.png'),
+//   require('../imgs/weather-icons/26.png'),
+//   require('../imgs/weather-icons/29.png'),
+//   require('../imgs/weather-icons/30.png'),
+//   require('../imgs/weather-icons/31.png'),
+//   require('../imgs/weather-icons/32.png'),
+//   require('../imgs/weather-icons/33.png'),
+//   require('../imgs/weather-icons/34.png'),
+//   require('../imgs/weather-icons/35.png'),
+//   require('../imgs/weather-icons/36.png'),
+//   require('../imgs/weather-icons/37.png'),
+//   require('../imgs/weather-icons/38.png'),
+//   require('../imgs/weather-icons/39.png'),
+//   require('../imgs/weather-icons/40.png'),
+//   require('../imgs/weather-icons/41.png'),
+//   require('../imgs/weather-icons/42.png'),
+//   require('../imgs/weather-icons/43.png'),
+//   require('../imgs/weather-icons/44.png')
+// ];
+
+class Weather extends React.Component {
+
+  getImagePath() {
+    console.log("../imgs/weather-icons/" + this.props.iconNumber + ".png");
+    return "../imgs/weather-icons/" + this.props.iconNumber + ".png";
+  }
+
+  render() {
+    return(
+      <div>
+        { this.props.name && this.props.area && this.props.country && <h4> Results for { this.props.name }, { this.props.area }, { this.props.country }</h4> }
+        { this.props.high && <p className="temperature"> High: { this.props.high } &deg;F</p> }
+        { this.props.low && <p className="temperature"> Low: { this.props.low } &deg;F</p> }
+        { this.props.iconNumber && <img id="weather-status-icon" src={this.props.weatherImages[this.props.iconNumber - 1]}/> }
+      </div>
+    );
+  }
 }
 
 export default Weather;
