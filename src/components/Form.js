bdbe1968cad279960ba30eaa9014c8ceda12ee13
ericong18 <ericong18@gmail.com>
2018-06-28 15:57:29 -0700

    Added white background to high and low temperature of five day forecast

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ const Form = props => {
   return (
     <form onSubmit={ props.weather }>
       <div className="input-group mb-3">
-        <input type="text" className="form-control" name="location" placeholder="City, State, Country..." size="10"/>
+        <input type="text" className="form-control" name="location" placeholder="Enter a place..." size="10" autocomplete="off"/>
         <button className="btn btn-primary">Submit</button>
       </div>
     </form>
--- src/components/WeatherFive.js
+++ src/components/WeatherFive.js
@@ -27,8 +27,10 @@ class WeatherFive extends React.Component {
             { this.props.fiveDays && <h5 className="day-night">Night</h5> }
             { this.props.fiveDays && <div className="weather-image-five-container"><img className="weather-status-five-icon" src={this.props.weatherImages[this.props.fiveDays[0].Night.Icon - 1]} alt="forecast icon"/></div> }
             { this.props.fiveDays && <h6 className="day-night-status">{ this.props.fiveDays[0].Night.IconPhrase }</h6> }
-            { this.props.fiveDays && <h6 className="five-temp high"><span className="high-low-fahr">{ this.props.fiveDays[0].Temperature.Maximum.Value } &deg;F</span> <span className="high-low-celsius">{ this.fahrenheitToCelsius(this.props.fiveDays[0].Temperature.Maximum.Value) } &deg;C</span></h6> }
-            { this.props.fiveDays && <h6 className="five-temp"><span className="high-low-fahr">{ this.props.fiveDays[0].Temperature.Minimum.Value } &deg;F</span> <span className="high-low-celsius">{ this.fahrenheitToCelsius(this.props.fiveDays[0].Temperature.Minimum.Value) } &deg;C</span></h6> }
+            <div className="high-low-container">
+              { this.props.fiveDays && <h6 className="five-temp high"><span className="high-low-fahr">{ this.props.fiveDays[0].Temperature.Maximum.Value } &deg;F</span> <span className="high-low-celsius">{ this.fahrenheitToCelsius(this.props.fiveDays[0].Temperature.Maximum.Value) } &deg;C</span></h6> }
+              { this.props.fiveDays && <h6 className="five-temp low"><span className="high-low-fahr">{ this.props.fiveDays[0].Temperature.Minimum.Value } &deg;F</span> <span className="high-low-celsius">{ this.fahrenheitToCelsius(this.props.fiveDays[0].Temperature.Minimum.Value) } &deg;C</span></h6> }
+            </div>
           </div>
           <div className="col-md-2">
             { this.props.fiveDays && <h6 className="day-of-week">Tomorrow</h6> }
@@ -38,8 +40,10 @@ class WeatherFive extends React.Component {
             { this.props.fiveDays && <h5 className="day-night">Night</h5> }
             { this.props.fiveDays && <div className="weather-image-five-container"><img className="weather-status-five-icon" src={this.props.weatherImages[this.props.fiveDays[1].Night.Icon - 1]} alt="forecast icon"/></div> }
             { this.props.fiveDays && <h6 className="day-night-status">{ this.props.fiveDays[1].Night.IconPhrase }</h6>}
-            { this.props.fiveDays && <h6 className="five-temp high"><span className="high-low-fahr">{ this.props.fiveDays[1].Temperature.Maximum.Value } &deg;F</span> <span className="high-low-celsius">{ this.fahrenheitToCelsius(this.props.fiveDays[1].Temperature.Maximum.Value) } &deg;C</span></h6> }
-            { this.props.fiveDays && <h6 className="five-temp"><span className="high-low-fahr">{ this.props.fiveDays[1].Temperature.Minimum.Value } &deg;F</span> <span className="high-low-celsius">{ this.fahrenheitToCelsius(this.props.fiveDays[1].Temperature.Minimum.Value) } &deg;C</span></h6> }
+            <div className="high-low-container">
+              { this.props.fiveDays && <h6 className="five-temp high"><span className="high-low-fahr">{ this.props.fiveDays[1].Temperature.Maximum.Value } &deg;F</span> <span className="high-low-celsius">{ this.fahrenheitToCelsius(this.props.fiveDays[1].Temperature.Maximum.Value) } &deg;C</span></h6> }
+              { this.props.fiveDays && <h6 className="five-temp low"><span className="high-low-fahr">{ this.props.fiveDays[1].Temperature.Minimum.Value } &deg;F</span> <span className="high-low-celsius">{ this.fahrenheitToCelsius(this.props.fiveDays[1].Temperature.Minimum.Value) } &deg;C</span></h6> }
+            </div>
           </div>
           <div className="col-md-2">
             { this.props.fiveDays && <h6 className="day-of-week">{ this.getDayOfWeek(2) }</h6> }
@@ -49,8 +53,10 @@ class WeatherFive extends React.Component {
             { this.props.fiveDays && <h5 className="day-night">Night</h5> }
             { this.props.fiveDays && <div className="weather-image-five-container"><img className="weather-status-five-icon" src={this.props.weatherImages[this.props.fiveDays[2].Night.Icon - 1]} alt="forecast icon"/></div> }
             { this.props.fiveDays && <h6 className="day-night-status">{ this.props.fiveDays[2].Night.IconPhrase }</h6>}
-            { this.props.fiveDays && <h6 className="five-temp high"><span className="high-low-fahr">{ this.props.fiveDays[2].Temperature.Maximum.Value } &deg;F</span> <span className="high-low-celsius">{ this.fahrenheitToCelsius(this.props.fiveDays[2].Temperature.Maximum.Value) } &deg;C</span></h6> }
-            { this.props.fiveDays && <h6 className="five-temp"><span className="high-low-fahr">{ this.props.fiveDays[2].Temperature.Minimum.Value } &deg;F</span> <span className="high-low-celsius">{ this.fahrenheitToCelsius(this.props.fiveDays[2].Temperature.Minimum.Value) } &deg;C</span></h6> }
+            <div className="high-low-container">
+              { this.props.fiveDays && <h6 className="five-temp high"><span className="high-low-fahr">{ this.props.fiveDays[2].Temperature.Maximum.Value } &deg;F</span> <span className="high-low-celsius">{ this.fahrenheitToCelsius(this.props.fiveDays[2].Temperature.Maximum.Value) } &deg;C</span></h6> }
+              { this.props.fiveDays && <h6 className="five-temp low"><span className="high-low-fahr">{ this.props.fiveDays[2].Temperature.Minimum.Value } &deg;F</span> <span className="high-low-celsius">{ this.fahrenheitToCelsius(this.props.fiveDays[2].Temperature.Minimum.Value) } &deg;C</span></h6> }
+            </div>
           </div>
           <div className="col-md-2">
             { this.props.fiveDays && <h6 className="day-of-week">{ this.getDayOfWeek(3) }</h6> }
@@ -60,8 +66,10 @@ class WeatherFive extends React.Component {
             { this.props.fiveDays && <h5 className="day-night">Night</h5> }
             { this.props.fiveDays && <div className="weather-image-five-container"><img className="weather-status-five-icon" src={this.props.weatherImages[this.props.fiveDays[3].Night.Icon - 1]} alt="forecast icon"/></div> }
             { this.props.fiveDays && <h6 className="day-night-status">{ this.props.fiveDays[3].Night.IconPhrase }</h6>}
-            { this.props.fiveDays && <h6 className="five-temp high"><span className="high-low-fahr">{ this.props.fiveDays[3].Temperature.Maximum.Value } &deg;F</span> <span className="high-low-celsius">{ this.fahrenheitToCelsius(this.props.fiveDays[3].Temperature.Maximum.Value) } &deg;C</span></h6> }
-            { this.props.fiveDays && <h6 className="five-temp"><span className="high-low-fahr">{ this.props.fiveDays[3].Temperature.Minimum.Value } &deg;F</span> <span className="high-low-celsius">{ this.fahrenheitToCelsius(this.props.fiveDays[3].Temperature.Minimum.Value) } &deg;C</span></h6> }
+            <div className="high-low-container">
+              { this.props.fiveDays && <h6 className="five-temp high"><span className="high-low-fahr">{ this.props.fiveDays[3].Temperature.Maximum.Value } &deg;F</span> <span className="high-low-celsius">{ this.fahrenheitToCelsius(this.props.fiveDays[3].Temperature.Maximum.Value) } &deg;C</span></h6> }
+              { this.props.fiveDays && <h6 className="five-temp low"><span className="high-low-fahr">{ this.props.fiveDays[3].Temperature.Minimum.Value } &deg;F</span> <span className="high-low-celsius">{ this.fahrenheitToCelsius(this.props.fiveDays[3].Temperature.Minimum.Value) } &deg;C</span></h6> }
+            </div>
           </div>
           <div className="col-md-2">
             { this.props.fiveDays && <h6 className="day-of-week">{ this.getDayOfWeek(4) }</h6> }
@@ -71,8 +79,10 @@ class WeatherFive extends React.Component {
             { this.props.fiveDays && <h5 className="day-night">Night</h5> }
             { this.props.fiveDays && <div className="weather-image-five-container"><img className="weather-status-five-icon" src={this.props.weatherImages[this.props.fiveDays[4].Night.Icon - 1]} alt="forecast icon"/></div> }
             { this.props.fiveDays && <h6 className="day-night-status">{ this.props.fiveDays[4].Night.IconPhrase }</h6>}
-            { this.props.fiveDays && <h6 className="five-temp high"><span className="high-low-fahr">{ this.props.fiveDays[4].Temperature.Maximum.Value } &deg;F</span> <span className="high-low-celsius">{ this.fahrenheitToCelsius(this.props.fiveDays[4].Temperature.Maximum.Value) } &deg;C</span></h6> }
-            { this.props.fiveDays && <h6 className="five-temp"><span className="high-low-fahr">{ this.props.fiveDays[4].Temperature.Minimum.Value } &deg;F</span> <span className="high-low-celsius">{ this.fahrenheitToCelsius(this.props.fiveDays[4].Temperature.Minimum.Value) } &deg;C</span></h6> }
+            <div className="high-low-container">
+              { this.props.fiveDays && <h6 className="five-temp high"><span className="high-low-fahr">{ this.props.fiveDays[4].Temperature.Maximum.Value } &deg;F</span> <span className="high-low-celsius">{ this.fahrenheitToCelsius(this.props.fiveDays[4].Temperature.Maximum.Value) } &deg;C</span></h6> }
+              { this.props.fiveDays && <h6 className="five-temp low"><span className="high-low-fahr">{ this.props.fiveDays[4].Temperature.Minimum.Value } &deg;F</span> <span className="high-low-celsius">{ this.fahrenheitToCelsius(this.props.fiveDays[4].Temperature.Minimum.Value) } &deg;C</span></h6> }
+            </div>
           </div>
         </div>
       </div>
