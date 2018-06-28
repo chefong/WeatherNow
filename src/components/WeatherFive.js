91541a16d49d0194fd100089a53d42853ccd2346
ericong18 <ericong18@gmail.com>
2018-06-27 22:57:39 -0700

    Displayed day of week

--- src/components/WeatherFive.js
+++ src/components/WeatherFive.js
@@ -1,13 +1,21 @@
 import React from "react";
 import './WeatherFive.css';
 
+const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
 class WeatherFive extends React.Component {
+
+  getDayOfWeek(num) {
+    let d = new Date(this.props.fiveDays[num].Date);
+    return DAYS[d.getDay()];
+  }
 
   render() {
     return (
       <div className="container-fluid">
         <div className="row justify-content-center">
           <div className="col-md-2">
+            { this.props.fiveDays && <h6 className="day-of-week">Today</h6> }
             { this.props.fiveDays && <h5 className="day-night">Day</h5> }
             { this.props.fiveDays && <div className="weather-image-five-container"><img className="weather-status-five-icon" src={this.props.weatherImages[this.props.fiveDays[0].Day.Icon - 1]} alt="forecast icon"/></div> }
             { this.props.fiveDays && <h6 className="day-night-status">{ this.props.fiveDays[0].Day.IconPhrase }</h6> }
@@ -16,6 +24,7 @@ class WeatherFive extends React.Component {
             { this.props.fiveDays && <h6 className="day-night-status">{ this.props.fiveDays[0].Night.IconPhrase }</h6>}
           </div>
           <div className="col-md-2">
+            { this.props.fiveDays && <h6 className="day-of-week">Tomorrow</h6> }
             { this.props.fiveDays && <h5 className="day-night">Day</h5> }
             { this.props.fiveDays && <div className="weather-image-five-container"><img className="weather-status-five-icon" src={this.props.weatherImages[this.props.fiveDays[1].Day.Icon - 1]} alt="forecast icon"/></div> }
             { this.props.fiveDays && <h6 className="day-night-status">{ this.props.fiveDays[1].Day.IconPhrase }</h6> }
@@ -24,6 +33,7 @@ class WeatherFive extends React.Component {
             { this.props.fiveDays && <h6 className="day-night-status">{ this.props.fiveDays[1].Night.IconPhrase }</h6>}
           </div>
           <div className="col-md-2">
+            { this.props.fiveDays && <h6 className="day-of-week">{ this.getDayOfWeek(2) }</h6> }
             { this.props.fiveDays && <h5 className="day-night">Day</h5> }
             { this.props.fiveDays && <div className="weather-image-five-container"><img className="weather-status-five-icon" src={this.props.weatherImages[this.props.fiveDays[2].Day.Icon - 1]} alt="forecast icon"/></div> }
             { this.props.fiveDays && <h6 className="day-night-status">{ this.props.fiveDays[2].Day.IconPhrase }</h6> }
@@ -32,6 +42,7 @@ class WeatherFive extends React.Component {
             { this.props.fiveDays && <h6 className="day-night-status">{ this.props.fiveDays[2].Night.IconPhrase }</h6>}
           </div>
           <div className="col-md-2">
+            { this.props.fiveDays && <h6 className="day-of-week">{ this.getDayOfWeek(3) }</h6> }
             { this.props.fiveDays && <h5 className="day-night">Day</h5> }
             { this.props.fiveDays && <div className="weather-image-five-container"><img className="weather-status-five-icon" src={this.props.weatherImages[this.props.fiveDays[3].Day.Icon - 1]} alt="forecast icon"/></div> }
             { this.props.fiveDays && <h6 className="day-night-status">{ this.props.fiveDays[3].Day.IconPhrase }</h6> }
@@ -40,6 +51,7 @@ class WeatherFive extends React.Component {
             { this.props.fiveDays && <h6 className="day-night-status">{ this.props.fiveDays[3].Night.IconPhrase }</h6>}
           </div>
           <div className="col-md-2">
+            { this.props.fiveDays && <h6 className="day-of-week">{ this.getDayOfWeek(4) }</h6> }
             { this.props.fiveDays && <h5 className="day-night">Day</h5> }
             { this.props.fiveDays && <div className="weather-image-five-container"><img className="weather-status-five-icon" src={this.props.weatherImages[this.props.fiveDays[4].Day.Icon - 1]} alt="forecast icon"/></div> }
             { this.props.fiveDays && <h6 className="day-night-status">{ this.props.fiveDays[4].Day.IconPhrase }</h6> }
